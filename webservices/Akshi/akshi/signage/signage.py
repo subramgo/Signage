6f0514fa4ce5d12309ea2de2755c6b40c339de15
Gopi Subramanian <gsubramanian@tycoint.com>
2018-09-05 10:39:15 -0400

    web services will now return the last 3 days

--- webservices/Akshi/akshi/signage/signage.py
+++ webservices/Akshi/akshi/signage/signage.py
@@ -1,33 +1,33 @@
 from flask import render_template, flash, redirect, Blueprint, request, send_from_directory, Response
 import json
-import numpy as np 
-import cv2
 from .models import  signage_db,FaceSignage
 from .ObjectTracking import track_objects
+from sqlalchemy.sql import func, text
 
-from sqlalchemy import func
 import sys
-import dlib
 from flask import current_app as app
 import os
 import time
 import json
 
 signage_api = Blueprint('signage', __name__, url_prefix='/api/v1/signage', template_folder = 'templates', static_folder = 'static')
-face_detector = dlib.get_frontal_face_detector()
 
 
 
 @signage_api.route('/', methods =['GET'])
 def signage_page():
-    signs = FaceSignage.query.order_by(FaceSignage.date_created.desc()).all()
+    #signs = FaceSignage.query.order_by(FaceSignage.date_created.desc()).filter( (func('now') - func(FaceSignage.date_created)) <= 3)
+    signs = FaceSignage.query.filter(  (  func.julianday(func.date('now')) - func.julianday(FaceSignage.date_created) )<=3 ).all()
+
+
     signs_json = json.dumps([sign.get_json() for sign in signs])
     return Response(signs_json, mimetype="application/json")
  
 
 @signage_api.route('/object_track',methods=['GET'])
 def object_track():
-    signs = FaceSignage.query.order_by(FaceSignage.date_created.desc()).all()
+    #signs = FaceSignage.query.order_by(FaceSignage.date_created.desc()).all()
+    signs = FaceSignage.query.filter(  (  func.julianday(func.date('now')) - func.julianday(FaceSignage.date_created) )<=3 ).all()
     signs_json = [sign.get_json() for sign in signs]
     object_log = track_objects(signs_json)
     return Response(json.dumps(object_log), mimetype="application/json")
