4aae44ffb5421294a625d01e16127ba7d5985eb6
Michael Stewart <statueofmike@gmail.com>
2018-09-05 18:03:00 +0000

    bugfixes to test before merge into master

--- Edge/singlerun.sh
+++ Edge/singlerun.sh
@@ -17,14 +17,14 @@ nohup python3 -u /home/pi/Signage/Edge/Player.py 1> ${FILE_TO_WATCH} 2>/dev/null
 sleep 5
 echo "Player loaded"
 
-FILE_TO_WATCH=/home/pi/Signage/webservice/Akshi/ws.log
+FILE_TO_WATCH=/home/pi/Signage/webservices/Akshi/ws.log
 SEARCH_PATTERN='0.0.0.0'
 rm -rf ${FILE_TO_WATCH}
-nohup sh /home/pi/Signage/webservice/Akshi/run.sh 1> ${FILE_TO_WATCH} 2>/dev/null &
+nohup sh /home/pi/Signage/webservices/Akshi/run.sh 1> ${FILE_TO_WATCH} 2>/dev/null &
 sleep 5
 echo "Web service loaded"
 
 
-sleep 15
-python3 /home/pi/Signage/Edge/client.py -g True  -c 0 -w http:michael:irkbin//127.0.0.1:5000/api/v1/signage/signage_upload  -l signage-analysis-demo -i signage-camera-1
+CAM_URI=rtsp://root:pass@192.168.1.168/usecondstream
+python3 /home/pi/Signage/Edge/client.py -g True  -c $CAM_URI -w http://michael:irkbin@127.0.0.1:5000/api/v1/signage/signage_upload  -l signage-analysis-demo -i signage-camera-1
 
