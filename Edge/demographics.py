c52189aa9489b6ca0a087e1459a933c85e649c15
subramgo <gopi.subramanian@gmail.com>
2019-02-15 14:50:35 -0500

    face cropping with pad and new demographics classifier added

--- Edge/demographics.py
+++ Edge/demographics.py
@@ -20,9 +20,11 @@ import warnings as _warnings
 _warnings.simplefilter("ignore")
 
 
+class DemographicsClassifier():
 
-class  DemographicsClassifier():
-    def __init__(self,logger,cfg):
+    def __init__(self, logger, cfg):
+
+
         self.logger = logger
         self.cfg = cfg
         if not cfg['enabled']:
@@ -30,53 +32,35 @@ class  DemographicsClassifier():
 
         else:
             logger.info("Demographics service is enabled.")
-            self.__cleanup()
-            gender_model, age_model = load_models()
-            self.gender_model = gender_model
-            self.age_model = age_model
-            self.graph = tf.get_default_graph()
 
-    def __predict(self,model,image,batch_size):
-        with self.graph.as_default():
-            return model.predict(image, batch_size=batch_size)
+            self.model = None
+            self.model_path = '/opt/signage/models/wide_resnet_age_gender.h5'
+            self.__load_model()
 
-    def __cleanup(self):
-        self.batch_size = 1
-        self.gender_target_size = (100,100)
-        self.age_target_size = (128, 128)
-        self.gender_filter = {0:'male',1:'female'}
-        #self.age_filter = {'(0, 2)' :0, '(4, 6)':1 , '(8, 12)':2, '(15, 20)':3,'(25, 32)':4,'(38, 43)':5, '(48, 53)':6, '(60, 100)':7}
-        self.age_filter = {0: '(0, 2)', 1:'(4, 6)' , 2:'(8, 12)', 3:'(15, 20)',4:'(25, 32)',5:'(38, 43)', 6:'(38, 43)', 7:'(38, 43)'}
+    def __load_model(self):
+      
+      self.model = load_model(self.model_path)
+      self.model._make_predict_function() 
 
-        self.x_test = None
-        self.preprocessed = None
-        self.y_test = None
-        self.scores = None
-        self.predictions = None
+    def process_faces(self, in_image):
+        
+        image = in_image.resize((64, 64))
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        results = self.model.predict(np.asarray(image))
 
-    def input_preprocessing(self,image,image_w, image_h):
-        """ Preprocessing to match the training conditions for this model.
-        Apply resize, reshape, other scaling/whitening effects.
-        image can be any image size greater than 100x100 and it will be resized
-        """
-        image = image * (1./255.)
-        resized = cv2.resize(image, (image_w, image_h))
-        return resized.reshape(1,image_w,image_h,3)
+        predicted_genders = results[0][0]
+        female_prob = np.round(predicted_genders[0],2)
+        male_prob   = np.round(predicted_genders[1],2)
+        ages = np.arange(0, 101).reshape(101, 1)
+        predicted_ages = results[1].dot(ages).flatten()
+        age = np.floor(predicted_ages[0])
 
-    def process_face(self,x_test,y_test,batch_size):
-        # Gender Prediction
-        w, h = self.gender_target_size[0], self.gender_target_size[1]
-        image = self.input_preprocessing(x_test, w,h)
-        predictions = self.__predict(self.gender_model,  image, batch_size)
-        idx = np.argmax(predictions)
-        gender = self.gender_filter[idx]
-
-        # Age Prediction
-        w, h = self.age_target_size[0], self.age_target_size[1]
-        image = self.input_preprocessing(x_test, w,h)
-        predictions = self.__predict(self.age_model,  image, batch_size)
-        idx = np.argmax(predictions)
-        age = self.age_filter[idx]
+        return_gender = None
+        if male_prob > female_prob:
+            return_gender = 'male'
+        else:
+            return_gender = 'female'
 
         return gender, age
 
@@ -85,7 +69,7 @@ class  DemographicsClassifier():
             measures = []
             for face in faces:
                 try:
-                    measures.append(self.process_face(x_test=np.array(face), y_test=None, batch_size=1))
+                    measures.append(self.process_face(in_image=face))
                 except e:
                     self.logger.error("face processing error "+str(e))
 
@@ -102,20 +86,4 @@ class  DemographicsClassifier():
         self.logger.info(rpt)
 
 
-def load_models():
-    print("Loading models...")
-    with open('/opt/signage/models/gender.json','r') as f:
-        json = f.read()
-    gender_model = model_from_json(json)
-    gender_model.load_weights('/opt/signage/models/gender.h5')
-    print("  - gender model loaded")
-
-    with open('/opt/signage/models/age.json','r') as f:
-        json = f.read()
-    age_model = model_from_json(json)
-    age_model.load_weights('/opt/signage/models/age.h5')
-    print("  - age model loaded")
-
-    print("All models loaded.")
-    return gender_model, age_model
 
--- Edge/faces.py
+++ Edge/faces.py
@@ -15,6 +15,8 @@ class FaceDetector:
 
         # for dlib detector
         frame = frame.convert('RGB')
+        width, height = frame.size
+
         frame = np.copy(np.asarray(frame))
         frame.setflags(write=True)
 
@@ -22,7 +24,8 @@ class FaceDetector:
         windows = []
         faces = []
         for i, d in enumerate(dets):
-            windows.append([d.left(), d.top(), d.right(), d.bottom()])
+            window = self._pad_box([d.left(), d.top(), d.right(), d.bottom()],width, height)
+            windows.append(window)
             cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (255, 0, 255), 2)
             faces.append(frame[d.top():d.bottom(), d.left():d.right()])
 
@@ -32,3 +35,16 @@ class FaceDetector:
             self.logger.info("No faces detected.")
 
         return (faces,windows)
+    
+    def _pad_box(self,box, width, height):
+        """Pad a box size for context."""
+        ratio = 0.1
+
+        w = int(ratio*(box[2]-box[0]))
+        h = int(ratio*(box[3]-box[1]))
+
+        return [max(box[0]-w,0)
+               ,max(box[1]-h,0)
+               ,min(box[2]+w,width)
+               ,min(box[3]+h,height)
+                ]
